9a97c2f0913763c9f9c6c7db6164cbc50f3f401b
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2018-06-20 19:28:20 +0530

    routes update

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,58 @@
 //     return view('welcome');
 // });
 
+/**
+ * Get the ACTIVE todo tasks.
+ */
 Route::get('/', function() {
     // code to fetch todo tasks
 });
 
-Route::get('/todo/page/{page?}', function($page = 1) {
+/**
+ * Get the ACTIVE todo tasks for a given page.
+ */
+Route::get('/todo/active/{page?}', function($page = 1) {
     // code to fetch the todo tasks on page = $page
 });
 
+/**
+ * Get the DONE todo tasks for a given page.
+ */
+Route::get('/todo/done/{page?}', function($page = 1) {
+    // code to fetch the todo tasks on page = $page
+});
+
+/**
+ * Get the DELETED todo tasks for a given page.
+ */
+Route::get('/todo/deleted/{page?}', function($page = 1) {
+    // code to fetch the todo tasks on page = $page
+});
+
+/**
+ * Get a specific todo task by id.
+ */
 Route::get('/todo/{id}', function($id) {
     // code to fetch todo task having id = $id
 });
 
+/**
+ * Create a new todo task.
+ */
 Route::post('/todo', function() {
     // code to create new todo task
 });
 
+/**
+ * Update a specific todo task by id.
+ */
 Route::put('/todo/{id}', function($id) {
     // code to update todo task having id = $id
 });
 
+/**
+ * Delete a specific todo task by id.
+ */
 Route::delete('/todo/{id}', function($id) {
     // code to delete todo task having id = $id
 });
